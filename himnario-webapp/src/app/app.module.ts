84ebb3232c632d2bfba1257a4232b22374df2def
echavez95 <ea.chavarria95@gmail.com>
2023-01-13 00:08:11 -0600

    added firebase for audio files

--- himnario-webapp/src/app/app.module.ts
+++ himnario-webapp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +18,16 @@ import { HymnComponent } from './components/hymn/hymn.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyB5Dvn3J8fNee9I3dOzZyskp2rFrP2OAtY",
+      authDomain: "himnario-webapp.firebaseapp.com",
+      projectId: "himnario-webapp",
+      storageBucket: "himnario-webapp.appspot.com",
+      messagingSenderId: "793689209222",
+      appId: "1:793689209222:web:47f2a86cf371d88507d407"
+    }),
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- himnario-webapp/src/app/components/hymn/hymn.component.ts
+++ himnario-webapp/src/app/components/hymn/hymn.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Hymn } from 'src/app/models/hymn';
 import { HymnsproviderService } from 'src/app/services/hymnsprovider.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Component({
   selector: 'app-hymn',
@@ -13,13 +14,26 @@ export class HymnComponent {
   SelectedHymn: Hymn
   NextHymn: number = 0
   PrevHymn: number = 0
+  audioLink: string
 
-  constructor(public hymnsService: HymnsproviderService, public route: ActivatedRoute)
+  constructor(public hymnsService: HymnsproviderService, 
+      public route: ActivatedRoute, 
+      public firestorage: AngularFireStorage)
   {
     this.route.paramMap.subscribe(params => {
       this.Number = parseInt(params.get('number') as string, 10);
       this.SelectedHymn = this.hymnsService.searchByNumber(this.Number);
+      this.getAudioFile();
       this.setNextLast();
+    });
+  }
+
+  getAudioFile() {
+    let filename = `${this.Number}.mp3`;
+    const ref = this.firestorage.ref(filename);
+    this.audioLink = "";
+    ref.getDownloadURL().subscribe((data) => {
+      this.audioLink = data;
     });
   }
 
